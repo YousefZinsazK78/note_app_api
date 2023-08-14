22384ac60bd2188b396a14f8d1301a334fa4f462
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-08-15 01:39:24 +0330

    add db connection into project

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-//TODO: make connection to database
-//TODO: make first json api end point
-//? create note / update note / read note / delete note
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "root"
+	passowrd = "13781378"
+	hostname = "127.0.0.1"
+	dbname   = "noteappdb"
+)
+
+// TODO: make connection to database
+// TODO: make first json api end point
+// ? create note / update note / read note / delete note
 func main() {
-	println("hello world")
-	fmt.Println("")
+	db, err := sql.Open("mysql", generateDsn())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println("database successfully connected!")
+}
+
+func generateDsn() string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, passowrd, hostname, dbname)
 }
